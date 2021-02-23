f1ecb954de8ab8944e27b8cbb32bba5971098799
Clemente Castejon <clementecastejon3@gmail.com>
2021-02-23 18:41:59 -0400

    Added avatar pictures

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -1,25 +1,27 @@
-import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthenticationService } from '../services/authentication.service';
-import { filter, map, take } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { CanLoad, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthenticationService } from "../services/authentication.service";
+import { filter, map, take } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AutoLoginGuard implements CanLoad {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  constructor(private authService: AuthenticationService, private router: Router){}
-  
-  canLoad(): Observable<boolean> {    
+  canLoad(): Observable<boolean> {
     return this.authService.isAuthenticated.pipe(
-      filter(val => val !== null), 
-      take(1), 
-      map(isAuthenticated => {
-        console.log('Found previous token, automatic login');
-        if (isAuthenticated) {     
-          this.router.navigateByUrl('/tabs', { replaceUrl: true });
-        } else {          
+      filter((val) => val !== null),
+      take(1),
+      map((isAuthenticated) => {
+        console.log("Found previous token, automatic login");
+        if (isAuthenticated) {
+          this.router.navigateByUrl("/main-menu", { replaceUrl: true });
+        } else {
           return true;
         }
       })
