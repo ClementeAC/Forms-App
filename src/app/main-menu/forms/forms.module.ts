d3ed7bc6d509e4508c0506e14e25c7b05e35f132
Clemente Castejon <clementecastejon3@gmail.com>
2021-03-02 21:46:22 -0400

    Add questions to existing form functionality added.

--- src/app/main-menu/forms/forms.module.ts
+++ src/app/main-menu/forms/forms.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { FormsPageRoutingModule } from './forms-routing.module';
+import { FormsPageRoutingModule } from "./forms-routing.module";
 
-import { FormsPage } from './forms.page';
+import { FormsPage } from "./forms.page";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FormsPageRoutingModule
+    FormsPageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [FormsPage]
+  declarations: [FormsPage],
 })
 export class FormsPageModule {}
--- src/app/main-menu/forms/forms.page.ts
+++ src/app/main-menu/forms/forms.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormsService } from "../../services/forms.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AlertController, LoadingController } from "@ionic/angular";
+import { Form, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: "app-forms",
@@ -9,17 +10,23 @@ import { AlertController, LoadingController } from "@ionic/angular";
   styleUrls: ["./forms.page.scss"],
 })
 export class FormsPage implements OnInit {
+  questionData: FormGroup;
   constructor(
     private formsService: FormsService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private alertController: AlertController,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private fb: FormBuilder
   ) {}
 
   admin: string;
   recipeId: string;
   title: "";
+  form = {
+    question_title: "",
+    question_type: "",
+  };
   questions = [];
   answers: [];
   answer = {
@@ -41,6 +48,18 @@ export class FormsPage implements OnInit {
         this.title = data[0].title_form;
         console.log(this.questions);
       });
+    });
+    this.questionData = this.fb.group({
+      form_id: "",
+      title_q: "Pregunta",
+      description_q: "",
+      value: "",
+      response_size: null,
+      required: false,
+      selection: false,
+      text: false,
+      numeric: false,
+      checklist: false,
     });
   }
 
@@ -151,5 +170,370 @@ export class FormsPage implements OnInit {
         await alert.present();
       }
     );
+  }
+
+  async addQuestion(form) {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    this.formsService.createQuestion(form).subscribe(
+      async (res) => {
+        await loading.dismiss();
+        this.questions.push(res[0]);
+      },
+      async (res) => {
+        await loading.dismiss();
+        const alert = await this.alertController.create({
+          header: "Menu failed to create",
+          message: res.error.error,
+          buttons: ["OK"],
+        });
+        await alert.present();
+      }
+    );
+  }
+
+  async addNewQuestion() {
+    const alert = await this.alertController.create({
+      header: "Add New Question",
+      inputs: [
+        {
+          name: "newQuestionName",
+          type: "text",
+          placeholder: "Question title",
+        },
+        {
+          name: "newQuestionDescription",
+          type: "text",
+          placeholder: "Question description",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Next",
+          handler: (alertData) => {
+            console.log("Confirm Ok");
+            this.questionData.value.title_q = alertData.newQuestionName;
+            this.questionData.value.description_q =
+              alertData.newQuestionDescription;
+            console.log(this.questionData.value);
+            this.questionType();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async questionType() {
+    const alert = await this.alertController.create({
+      header: "Select question type:",
+      inputs: [
+        {
+          name: "questionType",
+          type: "radio",
+          label: "Text",
+          value: 1,
+        },
+        {
+          name: "questionType",
+          type: "radio",
+          label: "Numeric",
+          value: 2,
+        },
+        {
+          name: "questionType",
+          type: "radio",
+          label: "Simple selection",
+          value: 3,
+        },
+        {
+          name: "questionType",
+          type: "radio",
+          label: "Multiple selection",
+          value: 4,
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Next",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.form_id = this.recipeId;
+            if (alertData == 1) {
+              console.log("Es texto");
+              this.questionData.value.text = true;
+              console.log(this.questionData.value);
+              this.newText();
+            } else {
+              if (alertData == 2) {
+                console.log("Es Numeric");
+                this.questionData.value.numeric = true;
+                console.log(this.questionData.value);
+                this.newNumeric();
+              } else {
+                if (alertData == 3) {
+                  console.log("Es Simple selection");
+                  this.questionData.value.selection = true;
+                  console.log(this.questionData.value);
+                  this.newSimple();
+                } else {
+                  if (alertData == 4) {
+                    console.log("Es multiple selection");
+                    this.questionData.value.checklist = true;
+                    console.log(this.questionData.value);
+                  }
+                }
+              }
+            }
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async newText() {
+    const alert = await this.alertController.create({
+      header: "Maximum response size",
+      inputs: [
+        {
+          name: "textSize",
+          type: "number",
+          placeholder: "",
+          label: "Response size",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Create question",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.response_size = alertData.textSize;
+            console.log(this.questionData.value);
+            console.log("questionfinished");
+            this.addForm();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async newNumeric() {
+    const alert = await this.alertController.create({
+      header: "Maximum response size",
+      inputs: [
+        {
+          name: "textSize",
+          type: "number",
+          placeholder: "",
+          label: "Response size",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Create question",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.response_size = alertData.textSize;
+            console.log(this.questionData.value);
+            console.log("questionfinished");
+            this.addForm();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async newSimple() {
+    const alert = await this.alertController.create({
+      header: "What are the options?",
+      inputs: [
+        {
+          name: "option1",
+          type: "text",
+          placeholder: "Option 1",
+        },
+        {
+          name: "option2",
+          type: "text",
+          placeholder: "Option 2",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Add another option",
+          handler: (alertData) => {
+            this.questionData.value.value =
+              alertData.option1 + "|" + alertData.option2;
+            this.Options();
+          },
+        },
+        {
+          text: "Create question",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.value =
+              alertData.option1 + "|" + alertData.option2;
+            console.log(this.questionData.value.value);
+            console.log("question done");
+            this.addForm();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async Options() {
+    const alert = await this.alertController.create({
+      header: "Add new option",
+      inputs: [
+        {
+          name: "option",
+          type: "text",
+          placeholder: "New option",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Add another option",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.value =
+              this.questionData.value.value + "|" + alertData.option;
+            this.Options();
+          },
+        },
+        {
+          text: "Create question",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.value =
+              this.questionData.value.value + "|" + alertData.option;
+            this.addForm();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async newMultiple() {
+    const alert = await this.alertController.create({
+      header: "What are the options?",
+      inputs: [
+        {
+          name: "option1",
+          type: "text",
+          placeholder: "Option 1",
+        },
+        {
+          name: "option2",
+          type: "text",
+          placeholder: "Option 2",
+        },
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            console.log("Confirm Cancel");
+          },
+        },
+        {
+          text: "Add another option",
+          handler: (alertData) => {
+            this.questionData.value.value =
+              alertData.option1 + "|" + alertData.option2;
+            this.Options();
+          },
+        },
+        {
+          text: "Create question",
+          handler: (alertData) => {
+            console.log(alertData);
+            this.questionData.value.value =
+              alertData.option1 + "|" + alertData.option2;
+            console.log(this.questionData.value.value);
+            console.log("question done");
+            this.addForm();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async addForm() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    this.formsService
+      .createQuestion(this.questionData.value)
+      .subscribe(async (res) => {
+        await loading.dismiss();
+      });
+    this.router.navigate(["./main-menu/forms/" + this.recipeId]);
+
+    async (res) => {
+      await loading.dismiss();
+      const alert = await this.alertController.create({
+        header: "Question failed to create",
+        message: res.error.error,
+        buttons: ["OK"],
+      });
+      await alert.present();
+    };
   }
 }
